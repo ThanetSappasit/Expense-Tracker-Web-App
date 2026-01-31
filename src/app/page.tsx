6b50d56bcfd34a206365a4925127a8c3142d43b0
Thanet Sappasit <thanet.sappasit@gmail.com>
2026-02-01 02:11:39 +0700

    [feat] setup shadcn and install components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 
+import { ArrowUpRightIcon } from "lucide-react"
+import { Button } from "@/shared/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex min-h-screen items-center justify-center bg-foreground font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-background dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -58,6 +61,14 @@ export default function Home() {
           >
             Documentation
           </a>
+          <div className="flex items-start gap-2">
+            <Button variant="outline" size="lg">
+              Large
+            </Button>
+            <Button size="icon-lg" aria-label="Submit" variant="outline">
+              <ArrowUpRightIcon />
+            </Button>
+          </div>
         </div>
       </main>
     </div>
